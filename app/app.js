0c8d7197a43d6051b43aa409b143b2a2e98272c8
Graham Harper <graham@hrpr.co>
2015-06-20 13:19:06 +0100

    Fixed indentation.

--- app/app.js
+++ app/app.js
@@ -4,19 +4,19 @@
 
 	var app = angular.module('eventsApp', ['ngRoute']);
 
-    app.config(function($routeProvider) {
+  app.config(function($routeProvider) {
 
-        $routeProvider.when('/', {
-            templateUrl: 'app/events/events.html',
-            controller: 'EventsCtrl'
-        }).when('/events/:id', {
-            templateUrl: 'app/events/singleEvent.html',
-            controller: 'EventsCtrl'
-        }).when('/admin', {
-            templateUrl: 'app/admin/admin.html',
-            controller: 'AdminCtrl'
-        });
-    });
+      $routeProvider.when('/', {
+          templateUrl: 'app/events/events.html',
+          controller: 'EventsCtrl'
+      }).when('/events/:id', {
+          templateUrl: 'app/events/singleEvent.html',
+          controller: 'EventsCtrl'
+      }).when('/admin', {
+          templateUrl: 'app/admin/admin.html',
+          controller: 'AdminCtrl'
+      });
+  });
 
 	//controller as syntax
 	app.controller('SampleController', function(){
