90ef2ec831c662f5851e6dfc03e553a9b9cda2db
Graham Harper <graham@hrpr.co>
2015-06-20 23:35:40 +0100

    Move the modules return block to end of function.

--- app/services/database.factory.js
+++ app/services/database.factory.js
@@ -8,25 +8,24 @@
     database.$inject = ['$firebaseArray'];
 
     function database($firebaseArray) {
-        
+
         console.log('Init DB connection');
-        
+
         var url = 'https://lannistereventsdb.firebaseio.com/';
-        
-        var factory = { 
+
+        function categories() {
+           return $firebaseArray(new Firebase(url + 'categories'));
+        }
+
+        function events() {
+           return $firebaseArray(new Firebase(url + 'events'));
+        }
+
+        return {
             categories: categories,
             events: events
         };
-        return factory;
-        
-        function categories() {
-           return $firebaseArray(new Firebase(url + 'categories'));  
-        }
-        
-        function events() {
-           return $firebaseArray(new Firebase(url + 'events'));  
-        }
-        
+
     }
 
 })();
